c6fd3c58906150bf5ade01b174f910f902acb4df
Kuba <kubaniedziela.zg@gmail.com>
2021-11-29 18:21:01 +0000

    added sqlalchemy, created database, added uid to database while new acc is created, solved sign up susccess issue

--- api/api.py
+++ api/api.py
@@ -1,22 +1,38 @@
 from flask import Flask
 from flask import *
-import sqlalchemy
-from db import Database
 from flask_cors import *
 import enciphering
 import deciphering
-# from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
-# db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users_contacts.db'
+db = SQLAlchemy(app)
 CORS(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    uid = db.Column(db.String(28), nullable=False)
+    contacts = db.relationship('Users_contact', backref='uid', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.uid}')"
+
+class Users_contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
+    surname = db.Column(db.String(60), nullable=False)
+    n = db.Column(db.Integer, nullable=False)
+    e = db.Column(db.Integer, nullable=False)
+    user_uid = db.Column(db.String(28), db.ForeignKey('user.uid'), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.name}', '{self.surname}', '{self.n}', '{self.e}')"
 
 
 @app.route('/')
 def home_page():
     return "PIGEONFY API: /message/encipher ; /message/decipher ; /contacts"
-
 
 @app.route('/message/encipher', methods=['POST'])
 def encipher_user_message():
@@ -27,7 +43,6 @@ def encipher_user_message():
     enciphered_message = enciphering.encipher(message, int(n), int(e))
     return jsonify(response=enciphered_message)
 
-
 @app.route('/message/decipher', methods=['POST'])
 def decipher_user_message():
     recieved_file = request.json
@@ -35,12 +50,36 @@ def decipher_user_message():
 
     deciphered_message = deciphering.decipher(message)
     return jsonify(response=deciphered_message)
-
-@app.route('/user-table', methods=['GET', 'POST'])
-def create_user_table():
+ 
+@app.route('/new-user', methods=['GET', 'POST'])
+def create_new_user():
     recieved_file = request.json
     user_uid = recieved_file['uid']
+    new_user = User(uid=user_uid)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify('New user created successfully')
 
+@app.route('/add-contact', methods=['GET', 'POST'])
+def add_contact():
+    recieved_file = request.json
+    user_uid = recieved_file['uid']
+    new_contact_name = recieved_file['name']
+    new_contact_surname = recieved_file['surname']
+    new_contact_n = recieved_file['n']
+    new_contact_e = recieved_file['e']
 
+    new_contact = Users_contact(
+        name=new_contact_name, 
+        surname=new_contact_surname,
+        n=new_contact_n,
+        e=new_contact_e,
+        user_uid=user_uid
+        )
+    
+    db.session.add(new_contact)
+    db.session.commit()
+
+    return jsonify('Contact added successfully')
 
 app.run()
--- src/services/signUp.js
+++ src/services/signUp.js
@@ -1,8 +1,8 @@
-import {createUserWithEmailAndPassword, sendEmailVerification, updateProfile} from "firebase/auth";
-import {auth, firestore} from "../firebase";
-import {createAsyncThunk} from "@reduxjs/toolkit";
-import {doc, setDoc} from "firebase/firestore";
+import { createUserWithEmailAndPassword, sendEmailVerification, updateProfile } from "firebase/auth";
+import { auth } from "../firebase";
+import { createAsyncThunk } from "@reduxjs/toolkit";
 import { toast } from "react-toastify";
+import axios from "axios";
 
 export const userCollectionName = "users"
 export const createNewUser = createAsyncThunk("signUp/createNewUser", async (newUser, {rejectWithValue}) => {
@@ -11,14 +11,24 @@ export const createNewUser = createAsyncThunk("signUp/createNewUser", async (new
         await updateProfile(userCredential.user, {
             displayName: `${newUser.name} ${newUser.surname}`
         })
-        const userDoc = doc(firestore, userCollectionName, userCredential.user.uid)
-        await setDoc(userDoc, {})
+
         sendEmailVerification(userCredential.user)
+
+        await axios.post('http://127.0.0.1:5000/new-user', {
+            uid: userCredential.user.uid
+        }).then(function (response) {
+            console.log(response);
+        }).catch(function (error) {
+            console.log(error);
+        });
+        
         toast.success('Account successfully created')
+
         return {
             newUser: newUser,
             firebaseUser: userCredential.user
         }
+        
     } catch (e) {
         toast.error((e.code).slice(5, e.code.length))
         return rejectWithValue(e)
